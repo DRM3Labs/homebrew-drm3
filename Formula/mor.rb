a2ebf31315caf09ae5be98a5a2fdf22c63d13eea
robertjchristian <860343+robertjchristian@users.noreply.github.com>
2026-03-20 08:33:05 +0000

    bump mor to v0.1.5

--- Formula/mor.rb
+++ Formula/mor.rb
@@ -4,17 +4,17 @@
 class Mor < Formula
   desc "Local AI gateway for Morpheus decentralized inference"
   homepage "https://drm3.network"
-  version "0.1.4"
+  version "0.1.5"
   license "LicenseRef-Proprietary"
 
   on_macos do
     on_arm do
       url "https://github.com/drm3labs/drm3-releases/releases/download/mor-v#{version}/mor-darwin-arm64"
-      sha256 "d0b1552ffbeb1fe4f955b564a9ea740f3cb23328f1b9908ca238d27fff63fa41"
+      sha256 "c0fdce8dc2e8a73463af4fb6ac7cc68747ebcc4182d68914d3af4aea4674c17b"
     end
     on_intel do
       url "https://github.com/drm3labs/drm3-releases/releases/download/mor-v#{version}/mor-darwin-amd64"
-      sha256 "eba6f56eeccfa72a856913d77f55c55d3ed927558984747813b6937af6632b05"
+      sha256 "70520d38fff4cc6511f36376bd82d8a1e35be8b59750a79991f04662acc67c62"
     end
   end
 
