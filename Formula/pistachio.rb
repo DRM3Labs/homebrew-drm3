4f4307070463fe4139518fc16f767c978ca3b0b3
Robert Christian <860343+robertjchristian@users.noreply.github.com>
2026-06-26 23:35:16 -0400

    pistachio 0.32.0

--- Formula/pistachio.rb
+++ Formula/pistachio.rb
@@ -1,30 +1,30 @@
 class Pistachio < Formula
   desc "Pistachio — DRM3 P2P inference client CLI"
   homepage "https://drm3.network"
-  version "0.30.0"
+  version "0.32.0"
   license :cannot_represent
   on_macos do
     on_arm do
       url "https://github.com/drm3labs/drm3-releases/releases/download/pistachio-v#{version}/pistachio-darwin-arm64",
           verified: "github.com/drm3labs/drm3-releases/"
-      sha256 "b31d9c821ddbfd010d724078ae8b61c2abb9e8c1b4865f97982a3e67380cfdbf"
+      sha256 "fefd126501115fb92461a9f759729eba2f9ba0a23d11f0f06e0031fac08d622f"
     end
     on_intel do
       url "https://github.com/drm3labs/drm3-releases/releases/download/pistachio-v#{version}/pistachio-darwin-amd64",
           verified: "github.com/drm3labs/drm3-releases/"
-      sha256 "7d8b2797df527e21d238e47f9d403c06087198ba22d941522a65348e63ef072b"
+      sha256 "7dcd88d94b8f8ec3e4e3b6435cb002b8f4a9eab93f794104667e447212afc810"
     end
   end
   on_linux do
     on_arm do
       url "https://github.com/drm3labs/drm3-releases/releases/download/pistachio-v#{version}/pistachio-linux-arm64",
           verified: "github.com/drm3labs/drm3-releases/"
-      sha256 "3c934a11cc364c66c764e48f15f688221209955854e3726d2a406f5c8478b833"
+      sha256 "ee7a031fea7a9da9e116cd6d6a59a757a68100aea21d103009aef5adc8b485f6"
     end
     on_intel do
       url "https://github.com/drm3labs/drm3-releases/releases/download/pistachio-v#{version}/pistachio-linux-amd64",
           verified: "github.com/drm3labs/drm3-releases/"
-      sha256 "30b73dd2f4dd0c0eb375be61640f998a3a04e3ed2a9a51ba277f73caec6d5c0c"
+      sha256 "5925a96a48fd962ab949f264f837518c749438ee84e3baeb255a459bf6a62798"
     end
   end
   def install
